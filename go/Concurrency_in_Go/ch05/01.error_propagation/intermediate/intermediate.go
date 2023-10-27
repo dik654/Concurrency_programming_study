8d0217e7d9124e6276c5b55e1dcfbfdb9f0e6563
daeikkim <12377dl@gmail.com>
2023-10-27 10:37:52 +0900

    error propagation

---
+++ go/Concurrency_in_Go/ch05/01.error_propagation/intermediate/intermediate.go
@@ -0,0 +1,20 @@
+package intermeidate
+
+import (
+	"os/exec"
+)
+
+type IntermediateErr struct {
+	error
+}
+
+func runJob(id string) error {
+	const jobBinPath = "/bad/job/binary"
+	isExecutable, err := isGloballyExec(jobBinPath)
+	if err != nil {
+		return err
+	} else if isExecutable == false {
+		return wrapError(nil, "job binary is not executable")
+	}
+	return exec.Command(jobBinPath, "--id="+id.Run())
+}
